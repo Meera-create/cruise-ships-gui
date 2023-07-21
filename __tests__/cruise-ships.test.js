cf7c460ac804c44328f0d15ed06db6fd3bfbb036
Meera A <mirunaalini.alagarajah@gmail.com>
2023-07-21 12:33:20 +0100

    dry code

--- __tests__/cruise-ships.test.js
+++ __tests__/cruise-ships.test.js
@@ -5,28 +5,30 @@ const Itinerary = require('../src/itinerary')
 const Port = require ('../src/port')
 
 
-describe("constructor for ship",()=>{
+describe("ship",()=>{
+    describe('with ports and an itinerary',()=>{
+        let iceland;
+        let singapore;
+        let tripItinerary;
+        let myShip;
+    
+        beforeEach(()=>{ 
+             iceland = new Port('Iceland');
+             singapore =new Port('Singapore');
+             tripItinerary = new Itinerary([iceland,singapore]);
+             myShip = new Ship (tripItinerary);
+        })
+
     test("can be instatiated",()=>{
-        const myPort = new Port ('Dover');
-        const myItinerary = new Itinerary([myPort]);
-        const myShip = new Ship(myItinerary);
-        expect(myShip).toBeInstanceOf(Object);
+           expect(myShip).toBeInstanceOf(Object);
 
     })
 
     test("the ship has a start point",()=>{
-        const portRome = new Port('Rome');
-        const itineraryRome = new Itinerary([portRome]);
-        const myShip = new Ship(itineraryRome);
-        expect(myShip.currentPort).toBe(portRome)
+           expect(myShip.currentPort).toBe(iceland)
     })
 
     test(" the ship can set sail",()=>{
-       
-        const iceland = new Port('Iceland')
-        const singapore =new Port('Singapore')
-        const tripItinerary = new Itinerary([iceland,singapore])
-        const myShip = new Ship (tripItinerary);
         myShip.currentPort=iceland;
         myShip.previousPort=null;
         myShip.toSetSail()
@@ -34,58 +36,33 @@ describe("constructor for ship",()=>{
         
         expect(myShip.currentPort).toBeFalsy();
         expect(myShip.previousPort).toBe(iceland);
-        //expect(iceland.ships).not.toContain(myShip);
+        expect(iceland.ships).not.toContain(myShip);
     })
 
     test('it can\'t set sail further than the last item on itinerary',()=>{
-        const paris = new Port ('Paris');
-        const seoul = new Port('Seoul');
-        const myItinerary=new Itinerary([paris,seoul]);
-        const myShip = new Ship (myItinerary);
         myShip.toSetSail();
         myShip.docks();
+
         expect(()=>myShip.toSetSail()).toThrowError('End of Itinerary reached!');
     })
 
     
     test('it docks at another port',()=>{ 
-        const peru = new Port ('Peru')
-        const china = new Port ('China');
-        const wholeItinerary = new Itinerary([peru,china])
-        const myShip = new Ship (wholeItinerary)
-
         myShip.toSetSail()
         myShip.docks()
-        
-        
       
-        expect(china.ships).toContain(myShip);
-        expect(myShip.previousPort).toBe(peru);
-        expect(myShip.currentPort).toBe(china);
+        expect(singapore.ships).toContain(myShip);
+        expect(myShip.previousPort).toBe(iceland);
+        expect(myShip.currentPort).toBe(singapore);
         
     })
-
+    
     test('new ship gets added to port on instatiation',()=>{
-        const fiji = new Port ('Fiji')
-        const myItinerary = new Itinerary([fiji])
-        const newShip= new Ship(myItinerary);
-
-        expect(fiji.ships).toContain(newShip)
+        expect(iceland.ships).toContain(myShip);
     })
+})
 })
 
 
-//object:ship, method:set sail, property:port
 
 
-
-  
-
-
-//object:ship
-//method: have
-//properties: startingport
-
-//object:ship
-//method:add
-//property:passengers
--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,37 +1,44 @@
+const { beforeAll } = require('jest-circus');
 const Ship = require('../src/cruise-ships');
 const Itinerary = require('../src/itinerary');
 const Port=require('../src/port')
 
-
+describe('port',()=>{
 describe('port constructor',()=>{
+        let london;
+        let myShip;
+        let myShip2;
+    beforeEach(()=>{
+        london=new Port('London');
+        myShip={};
+        myShip2={};
+        
+    })
+
+
+
     test('new port can be instantiated',()=>{
         expect(new Port()).toBeInstanceOf(Object);
     })
 
     test('port has a name property',()=>{
-        const London = new Port('London');
-        expect(London.name).toBeTruthy();
+         london = new Port('London');
+        expect(london.name).toBeTruthy();
     })
 
     test('port add a ship method',()=>{
-        const japan = new Port('Japan');
-        const myShip = {}
-        japan.addShip(myShip);
-        expect(japan.ships).toContain(myShip);
-        //no need to create a new ship instance since we just want to check port.ship can store 
-        //a collection on ships
+        london.addShip(myShip);
+        expect(london.ships).toContain(myShip);
+    
     })
 
     test('port remove a ship method',()=>{
-        const mexico = new Port('Mexico');
-        const myShip = {};
-        const myShip2 = {};
-
-        mexico.addShip(myShip);
-        mexico.addShip(myShip2);
-        mexico.ships=[myShip,myShip2]
-        mexico.removeShip(myShip2);
-        expect(mexico.ships).toEqual([myShip]);
+        london.addShip(myShip);
+        london.addShip(myShip2);
+        london.ships=[myShip,myShip2]
+        london.removeShip(myShip2);
+        expect(london.ships).toEqual([myShip]);
         
     })
+})
 })
